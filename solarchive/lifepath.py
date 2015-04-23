aed410621f12f0530a916285e8b073c76052f5de
Uğurcan Ergün <ugurcanergn@gmail.com>
2015-04-24 00:21:54 +0300

    Implemented steps 4 to 6

--- solarchive/lifepath.py
+++ solarchive/lifepath.py
@@ -1,34 +1,38 @@
 from solarchive.data import lifepath, targets, all_data
-from solarchive.utils import roll_d100, roll_d10, clear_background
+from solarchive.utils import roll_d100, roll_d10, clear_package, get_last_background, get_pp
 
 
 class Lifepath(object):
-    STEPS = 3
+    STEPS = 6
 
     def __init__(self):
         self.step = 1
+        self.next_step = None
         self.char = {}
         self.data = lifepath
 
     def start_path(self):
         for i in range(self.STEPS):
             self.char[self.step-1] = self.get_next_step()
+            if self.next_step:
+                self.step = self.next_step
+                self.next_step = None
 
     def get_table(self):
         return self.data.get(str(self.step))
 
     def get_next_step(self):
-        func = getattr(self, "step_{}".format(self.step))
+        func = getattr(self, "step_{}".format(self.step), lambda x: None)
         result = func(self.get_table())
         self.step += 1
         return result
 
     def get_result(self, table):
         table_type = table.get("type", "general")
-        if table_type in ("general", "background"):
-            return table["table"]
-        elif table_type == "branching":
+        if table_type == "branching":
             return table["action"]
+        else:
+            return table["table"]
 
     def find_result_index(self, values, roll):
         for i, v in enumerate(values):
@@ -74,10 +78,10 @@ class Lifepath(object):
     def step_2(self, table):
         lang_table = self.get_from_target(table["target"])
         return {
-                "title": table["title"],
-                "desc": table.get("desc", ""),
-                "result": self.roll_on_table(lang_table)
-            }
+            "title": table["title"],
+            "desc": table.get("desc", ""),
+            "result": self.roll_on_table(lang_table)
+        }
 
     def step_3(self, table):
         result, index = self.roll_on_table(table, with_index=True)
@@ -98,19 +102,94 @@ class Lifepath(object):
             morph = final_table["morph"][final_index]
             if type(morph) == dict:
                 morph = self.roll_on_table(morph)
-            next = final_table["morph"][final_index]
+            next = final_table["next"][final_index]
             if type(next) == dict:
                 next = self.roll_on_table(next)
             backgrounds[i] = {
                 "title": final_table["title"],
                 "desc": final_table["desc"][final_index],
-                "background": clear_background(background, result["select"]),
+                "background": clear_package(background, result["select"]),
                 "morph": morph,
                 # ONLY THE FINAL NEXT VALUE COUNTS
                 "next": next,
-            }
+                }
         return {
-                "title": table["title"],
-                "desc": table.get("desc", "")[index],
-                "result": backgrounds
+            "title": table["title"],
+            "desc": table.get("desc", "")[index],
+            "result": backgrounds
+        }
+
+    def step_4(self, table):
+        result = self.roll_on_table(table)
+        have_more = result["effect"].pop("next", False)
+        result_dict = {
+            "title": table["title"],
+            "desc": result.get("desc", ""),
+            "result": result["effect"],
+            "extra": {}
+        }
+        if have_more:
+            if "change_background":
+                next_table = self.get_from_target("3.2")
+                next_result = self.get_from_target("background")["street rat"]
+                next_index = 5
+                backgrounds = self.char[3]["result"]
+                last_bg_key = get_last_background(backgrounds)
+                pp = get_pp(backgrounds[last_bg_key]["background"])
+                self.char[3]["result"][last_bg_key] = {
+                    "title": next_table["title"],
+                    "desc": next_table["desc"][next_index],
+                    "background": clear_package(next_result, pp),
+                    "morph": next_table["morph"][next_index],
+                    "next": next_table["next"][next_index],
+                    }
+            else:
+                next_table = self.data[have_more]
+                next_result = self.roll_on_table(next_table)
+                result_dict["extra"].update({
+                    "title": next_table["title"],
+                    "desc": next_result.get("desc", ""),
+                    "result": next_result["effect"],
+                    })
+
+        return result_dict
+
+    #TODO Implement Advanced Age modifiers
+    def step_5(self, table):
+        result, index = self.roll_on_table(table, with_index=True)
+        if index == 0:
+            self.next_step = 8
+        age = result["age"]
+        return {
+            "title": table["title"],
+            "desc": table.get("desc", ""),
+            "result": age + roll_d10()
+        }
+
+    #TODO Continue testing STEP 6
+    def step_6(self, table):
+        result, index = self.roll_on_table(table, with_index=True)
+        if result["next"] == "prev":
+            backgrounds = self.char[3]["result"]
+            last_bg_key = get_last_background(backgrounds)
+            next_table = self.get_from_target(backgrounds[last_bg_key]["next"])
+        elif result["next"] == "6.1":
+            branch_table = self.get_from_target("6.1")
+            branch_result = self.roll_on_table(branch_table)
+            next_table = self.get_from_target(branch_result["next"])
+        else:
+            next_table = self.get_from_target(result["next"])
+        next_result, next_index = self.roll_on_table(next_table, with_index=True)
+        target_table = self.get_from_target(next_table["target"])
+        print target_table.keys(), next_result
+        package = target_table[next_result]
+        return {
+            "title": table["title"],
+            "desc": table.get("desc", "")[index],
+            "result": {
+                "title": next_table["title"],
+                "desc": next_result,
+                "result": package
+            },
+            "next": next_table["next"]
         }
--- solarchive/utils.py
+++ solarchive/utils.py
@@ -2,27 +2,34 @@ import random
 import solconfig
 
 
-def roll_d10():
+def roll_d10(with_bonus=False):
+    if with_bonus:
+      return 10 % random.randint(1, 10)
     return random.randint(1, 10)
 
 
-def roll_d100():
+def roll_d100(with_bonus=False):
+    if with_bonus:
+      return 100 % random.randint(1, 100)
     return random.randint(1, 100)
 
 
-def clear_background(background, select):
+def clear_package(package, select):
     select = str(select)
     selects = ["1", "3", "5"]
     selects.remove(select)
     for s in selects:
-        background.pop(s, 0)
-    return background
+        package.pop(s, 0)
+    return package
 
-# def roll_on_table(table):
-#     roll = roll_values(table["values"])
-#     step_type = table["type"]
-#     result = find_result(table["values"], roll)
-#     if step_type == "background":
-#         return "background", table["package"][result], table["morph"][result], table["next"][result]
-#     data_type = solconfig.data_types[step_type]
-#     return data_type, table[data_type][result]
+def get_last_background(backgrounds):
+    key_list = backgrounds.keys()
+    key_list.sort()
+    return key_list[-1]
+
+def get_pp(package):
+    key_list = package.keys()
+    key_list.sort()
+    selects = ["1", "3", "5"]
+    select = filter(lambda x: x in selects, key_list)
+    return select[0]

196790fc96d419c4ff8bd9de72e7833b1fa9042f
Uğurcan Ergün <ugurcanergn@gmail.com>
2015-11-25 18:23:01 +0200

    Gear can now be listed by categories

--- solarchive/apiv10/resources/gear.py
+++ solarchive/apiv10/resources/gear.py
@@ -1,19 +1,29 @@
 from flask_restful import Resource
 
+from solarchive.apiv10.resources.common import parser
 from solarchive import data
 
 
 class ListGear(Resource):
     data_source = data.gear
+    have_categories = True
 
     def get(self):
         items = self.data_source
+        args = parser.parse_args()
+        categorize = False
+        if args.category and self.have_categories:
+            args.category = args.category.replace("_", " ")
+            categorize = True
+
         if type(items) == dict:
             keys = items.keys()
             keys.sort()
             item_list = []
             for key in keys:
                 item = items[key]
+                if categorize and args.category not in item["category"]:
+                    continue
                 item.update({"name": key})
                 item_list.append(item)
             items = item_list
